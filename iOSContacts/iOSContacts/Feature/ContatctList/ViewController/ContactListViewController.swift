e94edd37b095d559dfb0264082c85329de607deb
Douglas Mendes Barreto <mendes-barreto@live.com>
2017-09-30 17:02:19 -0300

    New folder structure to storyboards

--- iOSContacts/iOSContacts/Feature/ContatctList/ViewController/ContactListViewController.swift
+++ iOSContacts/iOSContacts/Feature/ContatctList/ViewController/ContactListViewController.swift
@@ -12,12 +12,7 @@ final class ContactListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
