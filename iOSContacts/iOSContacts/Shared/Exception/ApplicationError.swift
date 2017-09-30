4fa6f9bc27e0f921077145947de392ac30ee6bdc
Douglas Mendes Barreto <mendes-barreto@live.com>
2017-09-30 18:58:04 -0300

    Add new exception to be throw when the appDelegate is not found

---
+++ iOSContacts/iOSContacts/Shared/Exception/ApplicationError.swift
@@ -0,0 +1,10 @@
+//
+// Created by Douglas Barreto on 30/09/17.
+// Copyright (c) 2017 Douglas. All rights reserved.
+//
+
+import Foundation
+
+enum ApplicationError: Error {
+    case appDelegateNotFound
+}
