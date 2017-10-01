56ea331d84d3668bd2add03c434869a16a0cec44
Douglas Mendes Barreto <mendes-barreto@live.com>
2017-10-01 13:57:06 -0300

    Add contact detail place holder

---
+++ iOSContacts/iOSContacts/Feature/ContactDetail/Controller/ContactDetailViewController.swift
@@ -0,0 +1,10 @@
+//
+// Created by Douglas Barreto on 01/10/17.
+// Copyright (c) 2017 Douglas. All rights reserved.
+//
+
+import UIKit
+
+final class ContactDetailViewController: UIViewController {
+
+}
